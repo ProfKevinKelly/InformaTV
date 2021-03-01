c03ff9f31648eb1225fb075f17b7844cf7d7e07e
liambartsch <spamemail02021999@yahoo.com>
2021-03-01 18:53:06 +0100

    Added working backend and front-end and scripts to run both concurrently

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,20 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Customers from './components/customers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React Express Starter</h1>
+        </header>
+        <Customers />
+      </div>
+    );
+  }
 }
 
 export default App;
