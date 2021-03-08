1f2b83a6805d66847e87c2b3c4d5a999a3ff75eb
Killian <kronan@tcd.ie>
2021-03-08 13:02:35 +0000

    added table gets
    added authentication

--- back_end/routes/API.js
+++ back_end/routes/API.js
@@ -2,11 +2,34 @@ var express = require('express');
 var router = express.Router();
 const db = require('../config');
 
-router.get('/testFetch', function(req, res, next) {
-	db.conn.query('SELECT * FROM test.testTable', function (error, results, fields) {
+router.get('/getCurator', function(req, res, next) {
+	db.conn.query('SELECT * FROM test.Curator', function (error, results, fields) {
 		if (error) throw error;
 		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
 	});
 });
-
+router.get('/getElderlyPortal', function(req, res, next) {
+	db.conn.query('SELECT * FROM test.ElderlyPortal', function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
+});
+router.get('/getReminders', function(req, res, next) {
+	db.conn.query('SELECT * FROM test.Reminders', function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
+});
+router.get('/getCloseCircle', function(req, res, next) {
+	db.conn.query('SELECT * FROM test.CloseCirlcle', function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
+});
+router.get('/getCloseCircleMessages', function(req, res, next) {
+	db.conn.query('SELECT * FROM test.CloseCirlcleMessages', function (error, results, fields) {
+		if (error) throw error;
+		res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+	});
+});
 module.exports = router;
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Auth0Provider} from '@auth0/auth0-react';
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+<Auth0Provider 
+domain={domain} clientId={clientID} redirectUri={window.location.origin}>
+    <App />
+</Auth0Provider>
+, document.getElementById('root'));
 registerServiceWorker();
