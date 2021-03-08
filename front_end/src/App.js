0ee606398f4f98f5c6fc61cf0009510009d51d40
Killian <kronan@tcd.ie>
2021-03-08 13:02:35 +0000

    added table gets
    added authentication

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customers from './components/customers';
+import LoginButton from './components/Authentication/Login/LoginButton';
+import LogoutButton from './components/Authentication/Logout/LogoutButton';
+import Profile from './components/Authentication/Profile/Profile';
 
 class App extends Component {
   render() {
@@ -11,6 +14,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">InformaTV</h1>
         </header>
+        <div>
+          <LoginButton/>
+          <LogoutButton/>
+          <Profile/>
+        </div>
         <Customers />
       </div>
     );
