6a380be4a9f437db3ce5c8c84863b8953ce3ff07
Zugidor <zugidor@tutanota.com>
2021-03-25 14:34:28 +0000

    Fixed some of curator and trusted circle views in react app

--- front_end/src/components/Curator/Curator.js
+++ front_end/src/components/Curator/Curator.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import '../../styles.css';
+import ControlPanel from '../images/ControlPanel.jpg';
+import ElderContent from '../images/ElderContent.jpg';
+import TCtext from '../images/TCtext.jpg';
 
 class Curator extends Component {
   constructor() {
@@ -17,61 +20,65 @@ class Curator extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Curator View</h1>
-        <br/>
-        {/* <!--Trusted Circle Members list--> */}
-        <div class="cvTopRow">
-          <div id="TCListTop" class="listHeader">
-            <h2>Trusted Circle Members</h2>
-            <input type="text" id="NewMemb" placeholder="Name (Relation)"/>
-            <span onclick="newCircleMemb()" class="Add">Add</span>
-          </div>
-          <ul id="TCMembList">
-            <li>Bobby (Friend)</li>
-            <li>Caolán (Son)</li>
-            <li>Leila (Daughter)</li>
-          </ul>
-        </div>
-        <div class="cvTopRow">
-          {/* <!--Elderly Person's video content--> */}
-          <img class="cvContent" src="images/ElderContent.jpg" alt="Placeholder1"/>
-          {/* <!--Panel for controlling what Elderly Person sees (maybe it needs its own page?)--> */}
-          <img class="cvPanel" src="images/ControlPanel.jpg" alt="Placeholder2"/>
-        </div>
-        <br/>
+      <div class="curator">
+        <body>
 
-        {/* <!--Reminders & Messages, as in trusted_circle.html--> */}
-        <div class="RemMsg">
-          <div class="Reminders">
-            <div id="RemListTop" class="listHeader">
-              <h2>Reminders</h2>
-              <input type="text" id="NewRem" placeholder="Reminder"/>
-              <span onclick="newReminder()" class="Add">Add</span>
+          <h1>Curator View</h1>
+          <br/>
+          {/* <!--Trusted Circle Members list--> */}
+          <div class="cvTopRow">
+            <div id="TCListTop" class="listHeader">
+              <h2>Trusted Circle Members</h2>
+              <input type="text" id="NewMemb" placeholder="Name (Relation)"/>
+              <span onclick="newCircleMemb()" class="Add">Add</span>
             </div>
-
-            <ul id="RemList">
-              <li>Call Linda</li>
-              <li>Max visits tomorrow</li>
+            <ul id="TCMembList">
+              <li>Bobby (Friend)</li>
+              <li>Caolán (Son)</li>
+              <li>Leila (Daughter)</li>
             </ul>
           </div>
+          <div class="cvTopRow">
+            {/* <!--Elderly Person's video content--> */}
+            <img class="cvContent" src={ElderContent} alt="Placeholder1"/>
+            {/* <!--Panel for controlling what Elderly Person sees (maybe it needs its own page?)--> */}
+            <img class="cvPanel" src={ControlPanel} alt="Placeholder2"/>
+          </div>
+          <br/>
 
-          <div class="Messages">
-            <div class="MsgT">
-              <h2>Messages</h2>
-              <img src="images/TCtext.jpg" alt="Placeholder3"/>
+          {/* <!--Reminders & Messages, as in trusted_circle.html--> */}
+          <div class="RemMsg">
+            <div class="Reminders">
+              <div id="RemListTop" class="listHeader">
+                <h2>Reminders</h2>
+                <input type="text" id="NewRem" placeholder="Reminder"/>
+                <span onclick="newReminder()" class="Add">Add</span>
+              </div>
+
+              <ul id="RemList">
+                <li>Call Linda</li>
+                <li>Max visits tomorrow</li>
+              </ul>
             </div>
 
-            <div class="MsgB">
-              <form action="">
-                <label for="newmsg">Send a message:</label>
-                <br/>
-                <input class="txtbox" type="text" id="newTextMsg" name="newTextMsg"/>
-                <input class="SndBtn" type="button" value="Send" onclick="alert('Message Sent')"/>
-              </form>
+            <div class="Messages">
+              <div class="MsgT">
+                <h2>Messages</h2>
+                <img src={TCtext} alt="Placeholder3"/>
+              </div>
+
+              <div class="MsgB">
+                <form action="">
+                  <label for="newmsg">Send a message:</label>
+                  <br/>
+                  <input class="txtbox" type="text" id="newTextMsg" name="newTextMsg"/>
+                  <input class="SndBtn" type="button" value="Send" onclick="alert('Message Sent')"/>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
+
+        </body>
       </div>
     );
   }
--- front_end/src/components/TrustedCircle/TrustedCircle.js
+++ front_end/src/components/TrustedCircle/TrustedCircle.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../../styles.css';
+import TCtext from '../images/TCtext.jpg';
 
 class TrustedCircle extends Component {
   constructor() {
@@ -17,51 +18,46 @@ class TrustedCircle extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Trusted Circle View</h1>
+      <div class="TCircle">
+        <body>
 
-        {/* <!--video content currently seen by Elderly Person--> */}
-        <iframe class="TCvid" width="517" height="221" src="https://www.youtube.com/embed/5yW-v7EQkKA" frameborder="0" allowfullscreen></iframe>
-        {/* <!--<img class="tcVid" src="images/TCvideo.jpg" alt="Placeholder1">--> */}
-        <br/>
-        {/* <!--Reminders & Messages--> */}
-        <div class="RemMsg">
-          
-          <div class="Reminders">
-            <div id="RemListTop" class="listHeader">
-              <h2>Reminders</h2>
-              <input type="text" id="NewRem" placeholder="Reminder"/>
-              <span onclick="newReminder()" class="Add">Add</span>
+          <h1>Trusted Circle View</h1>
+          {/* <!--Reminders & Messages--> */}
+          <div class="RemMsg">
+
+            <div class="Messages">
+              <div class="MsgT">
+                <h2>Messages</h2>
+                <img src={TCtext} alt="Placeholder2"></img>{/*<!--Messages Window-->*/}
+              </div>
+
+              <div class="MsgB">
+                <form action="">{/* <!--sending a message doesn't do anything atm--> */}
+                  <label for="newmsg">Send a message:</label>
+                  <br></br>
+                  <input class="txtbox" type="text" id="newTextMsg" name="newTextMsg"></input>
+                  <input class="SndBtn" type="button" value="Send" onclick="alert('Message Sent')"></input>
+                </form>
+              </div>
+            </div>
+            <br></br>
+            
+            <div class="Reminders">
+              <div id="RemListTop" class="listHeader">
+                <h2>Reminders</h2>
+                <input type="text" id="NewRem" placeholder="Reminder"></input>
+                <span /*onclick={newReminder()}*/ class="Add">Add</span>
+              </div>
+
+              <ul id="RemList">
+                <li>Call Linda</li>{/* <!--placeholder reminder--> */}
+                <li>Max visits tomorrow</li>{/* <!--placeholder reminder--> */}
+              </ul>
             </div>
 
-            <ul id="RemList">
-              <li>Call Linda</li>
-              {/* <!--placeholder reminder--> */}
-              <li>Max visits tomorrow</li>
-              {/* <!--placeholder reminder--> */}
-            </ul>
           </div>
 
-          <div class="Messages">
-            <div class="MsgT">
-              <h2>Messages</h2>
-              <img src="images/TCtext.jpg" alt="Placeholder2"/>
-                {/* <!--Messages Window--> */}
-            </div>
-
-            <div class="MsgB">
-              <form action="">
-                {/* <!--sending a message doesn't do anything atm--> */}
-                <label for="newmsg">Send a message:</label>
-                <br/>
-                <input class="txtbox" type="text" id="newTextMsg" name="newTextMsg"/>
-                <input class="SndBtn" type="button" value="Send" onclick="alert('Message Sent')"/>
-              </form>
-            </div>
-          </div>
-
-        </div>
-        
+        </body>
       </div>
     );
   }
