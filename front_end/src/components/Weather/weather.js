a5017a1dcb87f75d587bdf158a80ea011d7c3577
bartschliam <bartschl@tcd.ie>
2021-04-15 12:14:47 +0200

    Added humidity in weather API

--- front_end/src/components/Weather/weather.js
+++ front_end/src/components/Weather/weather.js
@@ -9,6 +9,7 @@ class Weather extends Component{
             temperature: "",
             city: "",
             country: "",
+            humidity: "",
             pressure: "",
             error: ""
         }
@@ -24,14 +25,15 @@ class Weather extends Component{
         const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${weatherApiKey}`)
 
         const response = await api_call.json();
-
+        console.log("Response = ", response);
 
         if(city && country && response.weather){
             this.setState({
                 icon: response.weather[0].icon,
-                temperature: response.main.temp,
+                temperature: response.main.temp.toFixed(0),
                 city: response.name,
                 country: response.sys.country,
+                humidity: response.main.humidity,
                 pressure: response.main.pressure,
                 error: ""
             })
@@ -46,14 +48,16 @@ class Weather extends Component{
     render(){
         return(
             <div>
-                {this.state.icon && <img src={`https://openweathermap.org/img/w/${this.state.icon}.png`} alt = "weather icon" width = "10%" 
+                {this.state.icon && <img src={`https://openweathermap.org/img/w/${this.state.icon}.png`} alt = "weather icon" width = "50%" 
                 height = "10%"/>}
                 {this.state.country && this.state.city && <p>Location:&nbsp; 
                 {this.state.city}</p>}
                 {this.state.temperature && <p> Temperature:&nbsp;
                 {this.state.temperature}&deg;C</p>}
+                {this.state.humidity && <p> Humidity:&nbsp;
+                {this.state.humidity}</p>}
                 {this.state.pressure && <p> Pressure:&nbsp;
-                {this.state.pressure}PA</p>}
+                {this.state.pressure} PA</p>}
                 {this.state.error &&<p>{this.state.error}</p>}
                 
             </div>
